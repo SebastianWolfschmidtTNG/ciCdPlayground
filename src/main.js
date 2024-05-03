3d6085f277edee3b74b698a00caa414d03c54fee
Sebastian Wolfschmidt <sebastian.wolfschmidt@tngtech.com>
2024-05-03 09:54:02 +0200

    added personalization

--- src/main.js
+++ src/main.js
@@ -4,25 +4,26 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
-		// In the following fiels you can either give a single string, 
+		name: 'Sebastian Wolfschmdit',
+		// In the following fields you can either give a single string,
 		// or an array of bullet points
-		
+
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Difficult to understand processes, especially if you have never heard of the inner workings before. Often fails for dubious reasons.'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
-		// What do you want to learn in this workshop? 
+		tools: 'Github, Gitlab Pipelines, JFrog',
+		// What do you want to learn in this workshop?
 		expectations: [
-			'what you are talking about'
+			'Learn about what is going on in CI/CD and get a high-level overview.'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'cycling',
+			'running',
+			'basketball'
 		]
 	}
 });
+
